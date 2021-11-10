73bd3266d17e7b433d703b9b2d5ec1cae1427253
Marco Túlio <marcotulio21mm/@gmail.com>
2021-11-10 17:46:05 -0300

    configuracoes

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>Inicio | ig.news</title>
+    </Head>
+    
+    <h1 className={styles.title}>
+
+      Hello World
+    </h1>
+    </>
+  )
+}
