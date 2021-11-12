ec1abf74ba466bc09479ded300c62756f0337aac
Marco Túlio <marcotulio21mm/@gmail.com>
2021-11-12 17:14:42 -0300

    conectando api e salvando operacoes no banco

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 
@@ -5,13 +8,37 @@ interface SubscribeButtonProps{
     priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps){
+export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+            const {sessionId} = response.data;
+            const stripe = await getStripeJs()
+            await stripe.redirectToCheckout({sessionId: sessionId})
+            
+        }catch (err){
+            alert(err.message);
+        }
+    }
+
     return (
         <button
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe now
         </button>
     );
+
 }
+
+
+    
